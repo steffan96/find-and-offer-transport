f089c0066e5d4f4830da7fa55d05bf49b5b4b554
steffan96 <stefanmilicic@yahoo.com>
2022-03-17 12:51:48 +0100

    sec key

--- findoffer/settings.py
+++ findoffer/settings.py
@@ -29,8 +29,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
-REFRESH_TOKEN_SECRET = env('SECRET_KEY')
+SECRET_KEY = env('SEC_KEY')
+REFRESH_TOKEN_SECRET = env('SEC_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
